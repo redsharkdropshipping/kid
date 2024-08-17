7e031ee7d2b52e465f59acd4189af72636cdc2ba
redsharkdropshipping <lukeyoon0514@gmail.com>
2024-08-17 23:49:12 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+       
